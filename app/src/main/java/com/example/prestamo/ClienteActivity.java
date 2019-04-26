4391c6f7fd348bb27a5a94648cee505cbe1b75b4
anabell <carballoanabell@gmail.com>
2019-04-25 20:10:11 -0600

    inicio

--- app/src/main/java/com/example/prestamo/ClienteActivity.java
+++ app/src/main/java/com/example/prestamo/ClienteActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,23 +14,28 @@ import java.util.List;
 
 public class ClienteActivity extends AppCompatActivity {
 
-   public  TextView nombre = findViewById(R.id.nombre);
-   public  TextView apellido = findViewById(R.id.apellido);
-   public TextView telefono =findViewById(R.id.telefono);
-   public  TextView cedula = findViewById(R.id.cedula);
-   public  TextView direccion = findViewById(R.id.direccion);
-   public  TextView ocupacion =  findViewById(R.id.ocupacion);
-   public  TextView sexo  = findViewById(R.id.sexo);
    public  int i=0;
-
+   public int temp=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cliente);
 
+        mostrar();
+
     }
     public  void mostrar (){
+
+         TextView nombre = findViewById(R.id.nombre);
+         TextView apellido = findViewById(R.id.apellido);
+        TextView telefono =findViewById(R.id.telefono);
+         TextView cedula = findViewById(R.id.cedula);
+        TextView direccion = findViewById(R.id.direccion);
+        TextView ocupacion =  findViewById(R.id.ocupacion);
+        TextView sexo  = findViewById(R.id.sexo);
+
+
         nombre.setText(Datos.clientes.get(i).getNombre());
         apellido.setText(Datos.clientes.get(i).getApellido());
         telefono.setText(Datos.clientes.get(i).getTelefono());
@@ -45,28 +51,28 @@ public class ClienteActivity extends AppCompatActivity {
             Intent intent1 = new Intent(ClienteActivity.this, Main2ActivityRegistro.class);
             //intent1.putExtra("res",nombre.getText().toString());
             startActivity(intent1);
-//        } else
-//            if (view.getId() == R.id.btnsig) {
-//                if (i < Datos.clientes.size()-1)
-//                    Toast.makeText(this, "Es5e es el ultimo registro", Toast.LENGTH_SHORT).show();
-//                else
-//                {
-//                    i= i +1;
-//                    mostrar();
-//                }
-//
-//            }
-//            else
-//                if (view.getId()== R.id.btnatras)
-//                {
-//                    if (i ==0 )
-//                        Toast.makeText(this, "Es5e es el primern registro", Toast.LENGTH_SHORT).show();
-//                    else
-//                    {
-//                        i= i -1;
-//                        mostrar();
-//                    }
-//                }
+        } else
+            if (view.getId() == R.id.btnsig) {
+                if (i < Datos.clientes.size()-1)
+                    Toast.makeText(this, "Es5e es el ultimo registro", Toast.LENGTH_SHORT).show();
+                else
+                {
+                    i= i +1;
+                    mostrar();
+                }
+
+            }
+            else
+                if (view.getId()== R.id.btnatras)
+                {
+                    if (i == temp )
+                        Toast.makeText(this, "Es5e es el primer registro", Toast.LENGTH_SHORT).show();
+                    else
+                    {
+                        i= i -1;
+                        mostrar();
+                    }
+                }
 
 
-        }}}
+        }}
